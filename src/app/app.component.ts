9d1950ef5ee64fed8476512e36e49169aac88a2b
kathir3000 <kathir3000@gmail.com>
2019-04-27 22:08:16 +0530

    map with marker working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import * as d3 from "d3"
-import {topology, feature} from 'topojson';
+import * as topojson from "topojson";
 
 @Component({
   selector: 'app-root',
@@ -11,54 +11,50 @@ export class AppComponent {
   title = 'D3Maps';
   ngAfterContentInit() {
     var height = 600;
-        var width = 900, centered;
+    var width = 900, centered;
 
-        var projection = d3.geo.albersUsa();
-        var path = d3.geo.path().projection(projection);
+    var projection = d3.geoAlbersUsa();
+    var path = d3.geoPath().projection(projection);
 
-        var svg = d3.select("#map")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
+    var svg = d3.select("#map")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+ 
+    var g = svg.append("g");
 
-        var g = svg.append("g");
+    d3.json('./assets/us.json').then(function (us:any) {
+      console.log("usa", us);
 
-        d3.json('us.json', function(us) {
-            console.log("usa", us);
+      var map = svg.append('g') //.attr('class', 'boundary');
+      var usa = map.selectAll('path')
+               .data(topojson.feature(us, us.objects.states).features);
 
-            var map = svg.append('g').attr('class', 'boundary');
-            usa = map.selectAll('path')
-                     .data(topojson.feature(us, us.objects.states).features);
-
-            usa.enter()
-                    .append('path')
-                    .attr('d', path)
-                    .attr('fill', 'gray');
-
-
-        d3.json('newstorelocations.json', function (locations){
-            console.log('stores', locations);
-
-           svg.selectAll('circle')
-                    .data(locations)
-                    .enter()
-                    .append('circle')
-                    .attr('cx', function(d) {return projection([d.lon, d.lat])[0]})
-                    .attr('cy', function(d) {return projection([d.lon, d.lat])[1]})
-                    .attr('r', 4)
-                   .on("mouseover", function(b){
-                       console.log("binish", b)
-                       d3.select(this).style("fill", "red").append('text')
-                       .text("hi");
-                   })
-                   .on("mouseout", function(){d3.select(this).style("fill", "blue");
-                   });
+      usa.enter()
+              .append('path')
+              .attr('d', path) 
+        .attr('fill', 'rgb(153, 255, 162)')
+        .attr('stroke','red');
 
 
+      d3.json('./assets/newstorelocations.json').then(function (locations:any) {
+        console.log('stores', locations);
 
-        });
-        });
-
-
-    }
+         svg.selectAll('circle')
+                  .data(locations)
+                  .enter()
+                  .append('circle')
+                  .attr('cx', function (d: any) {return projection([d.lon, d.lat])[0]})
+                  .attr('cy', function (d: any) {console.log('d',d);return projection([d.lon, d.lat])[1]})
+                  .attr('r', 4)
+                 .on("mouseover", function(b){
+                     console.log("b", b)
+                     d3.select(this).style("fill", "red").append('text')
+                     .text("hi");
+                 })
+                 .on("mouseout", function(){d3.select(this).style("fill", "blue");
+                 });
+      });
+    });
+  }
 }
